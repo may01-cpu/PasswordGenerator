7f6781230d0cae1c7913ddd94f07304b9792ec2c
unknown <mayasbey16@gmail.com>
2025-06-28 15:14:09 +0100

    created the pwd generator with simple design

---
+++ passwordGenerator/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace passwordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbPwd = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(500, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(342, 135);
+            this.maskedTextBox1.Mask = "00000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(199, 38);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.Validating += new System.ComponentModel.CancelEventHandler(this.maskedTextBox1_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(120, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(564, 62);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password Generator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter the length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(175, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // lbPwd
+            // 
+            this.lbPwd.AutoSize = true;
+            this.lbPwd.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPwd.Location = new System.Drawing.Point(336, 277);
+            this.lbPwd.Name = "lbPwd";
+            this.lbPwd.Size = new System.Drawing.Size(49, 31);
+            this.lbPwd.TabIndex = 6;
+            this.lbPwd.Text = ".....";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            " SamallLetter",
+            " CapitalLetter",
+            " Digit",
+            " MixChars",
+            " SpecialCharacter"});
+            this.comboBox1.Location = new System.Drawing.Point(342, 211);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(199, 28);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkRed;
+            this.button2.Location = new System.Drawing.Point(131, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 75);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Generate Key";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(125, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Choose a type";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbPwd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbPwd;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ passwordGenerator/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum enCharType
+        {
+            SamallLetter = 0, CapitalLetter = 1,
+            Digit = 2, MixChars = 3, SpecialCharacter = 4
+        };
+
+        private static readonly Random rand = new Random();
+        static int RandomNumber(int From, int To)
+        {
+     
+             return rand.Next(From, To + 1); 
+       
+        }
+        static char GetRandomCharacter(enCharType CharType)
+        {
+
+            //updated this method to accept mixchars
+            if (CharType==enCharType.MixChars)
+            {
+                //Capital/Samll/Digits only
+                CharType = (enCharType)RandomNumber(1, 3);
+
+            }
+
+            switch (CharType)
+            {
+
+                case enCharType.SamallLetter:
+                    {
+                        return (char)(RandomNumber(97, 122));
+                       
+                    }
+                case enCharType.CapitalLetter:
+                    {
+                        return (char)(RandomNumber(65, 90));
+                        
+                    }
+                case enCharType.SpecialCharacter:
+                    {
+                        return (char)(RandomNumber(33, 47));
+                       
+                    }
+                case enCharType.Digit:
+                    {
+                        return (char)(RandomNumber(48, 57));
+                       
+                    }
+          
+                default:
+                    {
+                        return (char)(RandomNumber(65, 90));
+                       
+                    }
+            }
+        }
+        static string GenerateWord(enCharType CharType, int Length)
+
+        {
+
+            string Word = "";
+           
+                for (int i = 1; i <= Length; i++)
+
+                {
+
+                    Word += GetRandomCharacter(CharType);
+
+                }
+            
+         
+            return Word;
+        }
+        static string GenerateKey(enCharType CharType =enCharType.CapitalLetter)
+        {
+
+            string Key = "";
+
+
+            Key = GenerateWord(CharType, 4) + "-";
+            Key = Key + GenerateWord(CharType, 4) + "-";
+            Key = Key + GenerateWord(CharType, 4) + "-";
+            Key = Key + GenerateWord(CharType, 4);
+
+
+            return Key;
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != null)
+            {
+                lbPwd.Text = GenerateWord((enCharType)comboBox1.SelectedIndex, Convert.ToInt32( maskedTextBox1.Text));
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            lbPwd.Text = GenerateKey((enCharType)comboBox1.SelectedIndex);
+        }
+
+       
+
+        private void maskedTextBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(maskedTextBox1.Text))
+            {
+                e.Cancel = true;
+               maskedTextBox1.Focus();
+                errorProvider1.SetError(maskedTextBox1, "the length should have a value!");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(maskedTextBox1, "");
+            }
+        }
+    }
+}
